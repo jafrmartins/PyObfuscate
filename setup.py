0c9494e71bc03ddcc0118c268da10e354454a897
saubi <saubi@contoso.local>
2024-12-20 11:06:08 +0000

    published

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 import setuptools
 
 # pip install ../../Azure/PyObfuscate --upgrade -t ./.venv/lib/python3.10/site-packages/PyObfuscate
-  
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+
 setuptools.setup(
-    name='PyObfuscate',
-    version='0.1',
+    name='PySimpleObfuscate',
+    version='0.2',
     description='A Simple Python Obfuscator',
     packages=setuptools.find_packages('src'),
     install_requires=[],
@@ -15,4 +19,7 @@ setuptools.setup(
         ],
     },
     include_package_data=True,
+    # other arguments omitted
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
